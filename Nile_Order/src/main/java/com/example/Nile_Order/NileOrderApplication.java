a96097fa0ca816aa2f40d2cf7c21b7a5e1df8685
Nilesh Sodha <nilesh.sodha@coviam.com>
2018-06-12 14:59:27 +0530

    Initial commit

---
+++ Nile_Order/src/main/java/com/example/Nile_Order/NileOrderApplication.java
@@ -0,0 +1,12 @@
+package com.example.Nile_Order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NileOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NileOrderApplication.class, args);
+	}
+}
